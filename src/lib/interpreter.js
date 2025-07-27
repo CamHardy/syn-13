d9df67028e6c44363277da769dff6da1e4663d80
Cam Hardy <camfhardy@gmail.com>
2025-07-27 15:59:31 -0600

    added interpreter

---
+++ src/lib/interpreter.js
@@ -0,0 +1,182 @@
+import { System } from './system.js';
+
+/** @import { Token } from './token.js' */
+/** @import { Literal, Grouping, Unary, Binary, ExpressionType } from './expression.js' */
+/** @import { Expression, Print, StatementType } from './statement.js' */
+
+export class Interpreter {
+	/** @param { StatementType[] } statements */
+	interpret(statements) {
+		try {
+			for (const statement of statements) {
+				this.#visit(statement, this);
+			}
+		} catch (error) {
+			if (error instanceof RuntimeError) {
+				System.runtimeError(error);
+			}
+			throw error;
+		}
+	}
+	/** @param { Literal } node */
+	Literal(node) {
+		return node.value;
+	}
+
+	/** @param { Grouping } node */
+	Grouping(node) {
+		return this.#visit(node.expression, this);
+	}
+
+	/** @param { Unary } node */
+	Unary(node) {
+		const right = this.#visit(node.right, this);
+		switch (node.operator.type) {
+			case 'BANG':
+				return !this.#isTruthy(right);
+			case 'MINUS':
+				this.#checkNumberOperand(node.operator, right);
+				return -Number(right);
+		}
+
+		return null;
+	}
+
+	/** @param { Binary } node */
+	Binary(node) {
+		const left = this.#visit(node.left, this);
+		const right = this.#visit(node.right, this);
+
+		switch (node.operator.type) {
+			case 'GREATER':
+				this.#checkNumberOperands(node.operator, left, right);
+				return Number(left) > Number(right);
+			case 'GREATER_EQUAL':
+				this.#checkNumberOperands(node.operator, left, right);
+				return Number(left) >= Number(right);
+			case 'LESS':
+				this.#checkNumberOperands(node.operator, left, right);
+				return Number(left) < Number(right);
+			case 'LESS_EQUAL':
+				this.#checkNumberOperands(node.operator, left, right);
+				return Number(left) <= Number(right);
+			case 'MINUS':
+				this.#checkNumberOperands(node.operator, left, right);
+				return Number(left) - Number(right);
+			case 'PLUS':
+				if (typeof left === 'number' && typeof right === 'number') {
+					return Number(left) + Number(right);
+				}
+
+				if (typeof left === 'string' && typeof right === 'string') {
+					return String(left) + String(right);
+				}
+
+				throw new Error('Operands must be two numbers or two strings.');
+			case 'SLASH':
+				this.#checkNumberOperands(node.operator, left, right);
+				return Number(left) / Number(right);
+			case 'STAR':
+				this.#checkNumberOperands(node.operator, left, right);
+				return Number(left) * Number(right);
+			case 'BANG_EQUAL':
+				return this.#isEqual(left, right);
+			case 'EQUAL_EQUAL':
+				return this.#isEqual(left, right);
+		}
+
+		return null;
+	}
+
+
+	/** @param { Expression } node */
+	Expression(node) {
+		this.#visit(node.expression, this);
+
+		return null;
+	}
+
+	/** @param { Print } node */
+	Print(node) {
+		const value = this.#visit(node.expression, this);
+		console.log(this.#stringify(value));
+
+		return null;
+	}
+
+	/**
+	 * @param { Token } operator 
+	 * @param { any } operand 
+	 */
+	#checkNumberOperand(operator, operand) {
+		if (typeof operand === 'number') return;
+
+		throw new RuntimeError(operator, 'Operand must be a number.');
+	}
+
+	/**
+	 * @param { Token } operator 
+	 * @param { any } left 
+	 * @param { any } right 
+	 */
+	#checkNumberOperands(operator, left, right) {
+		if (typeof left === 'number' && typeof right === 'number') return;
+
+		throw new RuntimeError(operator, 'Operands must be numbers.');
+	}
+
+	/** @param { any } value */
+	#isTruthy(value) {
+		if (value === null) return false;
+		if (typeof value === 'boolean') return value;
+		return true;
+	}
+
+	/** 
+	 * @param { any } left 
+	 * @param { any } right 
+	 */
+	#isEqual(left, right) {
+		if (left === null && right === null) return true;
+		if (left === null) return false;
+
+		return left === right;
+	}
+
+	/**
+	 * @param { ExpressionType | StatementType } element 
+	 * @param {*} visitor
+	 */
+	#visit(element, visitor) {
+		if (!element || !visitor[element.type]) throw new Error(`No visitor for element type: ${element.type}`);
+
+		return visitor[element.type](element);
+	}
+
+	/** @param { any } value */
+	#stringify(value) {
+		if (value === null) return 'nil';
+
+		if (typeof value === 'number') {
+			let text = String(value);
+			if (text.endsWith('.0')) {
+				text = text.slice(0, -2);
+			}
+
+			return text;
+		}
+		
+		return String(value);
+	}
+}
+
+export class RuntimeError extends Error {
+	/**
+	 * @param { Token } token 
+	 * @param { string } message 
+	 */
+	constructor(token, message) {
+		super(message);
+		this.token = token;
+	}
+}
