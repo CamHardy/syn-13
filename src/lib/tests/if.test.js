6c3883a8cc6a70c6c59586d66b198183292c880d
Cam Hardy <camfhardy@gmail.com>
2025-08-28 15:30:02 -0600

    added if statement tests

--- src/lib/tests/if.test.js
+++ src/lib/tests/if.test.js
@@ -14,4 +14,57 @@ describe('If Statements', () => {
     System.run('if (false) print 1; else print 2;');
     expect(consoleMock).lastCalledWith('2');
   });
+
+  it('class in else', () => {
+    System.run('if (true) "ok"; else class Foo {}');
+    expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'class': Expected expression."));
+  });
+
+  it('class in then', () => {
+    System.run('if (false) class Foo {}');
+    expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'class': Expected expression."));
+  });
+
+  it('dangling else', () => {
+    System.run(`
+      if (true) if (false) print "bad"; else print "good";
+      if (false) if (true) print "bad"; else print "bad";
+    `);
+    expect(consoleMock).toHaveBeenCalledOnce();
+    expect(consoleMock).lastCalledWith('good');
+  });
+
+  it('else', () => {
+    System.run(`
+      if (true) print "good"; else print "bad";
+      if (false) print "bad"; else print "good";
+      if (false) nil; else { print "block"; }
+    `);
+    expect(consoleMock).nthCalledWith(1, 'good');
+    expect(consoleMock).nthCalledWith(2, 'good');
+    expect(consoleMock).nthCalledWith(3, 'block');
+  });
+
+  it('function in else', () => {
+    System.run('if (true) "ok"; else fun foo() {}');
+    expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'fun': Expected expression."));
+  });
+
+  it('function in then', () => {
+    System.run('if (true) fun foo() {}');
+    expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'fun': Expected expression."));
+  });
+
+  it('if', () => {
+    System.run(`
+      if (true) print "good";
+      if (false) print "bad";
+      if (true) { print "block"; }
+      var a = false;
+      if (a = true) print a;
+    `);
+    expect(consoleMock).nthCalledWith(1, 'good');
+    expect(consoleMock).nthCalledWith(2, 'block');
+    expect(consoleMock).nthCalledWith(3, 'true');
+  });
 });
