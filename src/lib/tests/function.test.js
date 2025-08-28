6b58ed2c444c46b9a473220b8d24902a179d7c20
Cam Hardy <camfhardy@gmail.com>
2025-08-27 22:33:39 -0600

    added function tests

--- src/lib/tests/function.test.js
+++ src/lib/tests/function.test.js
@@ -17,4 +17,73 @@ describe('Functions', () => {
     `);
     expect(consoleMock).lastCalledWith('7');
   });
+
+  it('body must be block', () => {
+    System.run('fun f() 123;');
+    expect(consoleMock).nthCalledWith(1, expect.stringContaining("Error at '123': Expected '{' before function body."));
+  });
+
+  it('empty body', () => {
+    System.run(`
+      fun f() {}
+      print f();
+    `);
+    expect(consoleMock).lastCalledWith('nil');
+  });
+
+  it('extra arguments', () => {
+    expect(() => System.run(`
+      fun f(a, b) {
+        print a;
+        print b;
+      }
+
+      f(1, 2, 3, 4);
+    `)).toThrowError('Expected 2 arguments but got 4.');
+  });
+
+  it('local mutual recursion', () => {
+    expect(() => System.run(`
+      {
+        fun isEven(n) {
+          if (n == 0) return true;
+          return isOdd(n - 1);
+        }
+
+        fun isOdd(n) {
+          if (n == 0) return false;
+          return isEven(n - 1);
+        }
+
+        isEven(4);
+      }
+    `)).toThrowError("Undefined variable 'isOdd'.");
+  });
+
+  it('local recursion', () => {
+    System.run(`
+      {
+        fun fib(n) {
+          if (n < 2) return n;
+          return fib(n - 1) + fib(n - 2);
+        }
+
+        print fib(8);
+      }
+    `);
+    expect(consoleMock).lastCalledWith('21');
+  });
+
+  it('missing arguments', () => {
+    expect(() => System.run(`
+      fun f(a, b) {}
+
+      f(1);
+    `)).toThrowError('Expected 2 arguments but got 1.');
+  });
+
+  it('missing comma in parameters', () => {
+    System.run('fun foo(a, b c, d, e, f) {}');
+    expect(consoleMock).nthCalledWith(1, expect.stringContaining("Error at 'c': Expected ')' after function parameters."));
+  });
 });
