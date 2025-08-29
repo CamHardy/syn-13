efd01226b8ae207f553b0b95e141696447e59f5b
Cam Hardy <camfhardy@gmail.com>
2025-08-29 16:58:55 -0600

    added while loop tests

--- src/lib/tests/while.test.js
+++ src/lib/tests/while.test.js
@@ -16,4 +16,61 @@ describe('While Loops', () => {
     expect(consoleMock).nthCalledWith(4, '3');
     expect(consoleMock).nthCalledWith(5, '4');
   });
+
+  it('class in body', () => {
+    System.run('while (true) class Foo {}');
+    expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'class': Expected expression."));
+  });
+
+  it('closure in body', () => {
+    System.run(`
+      var f1;
+      var f2;
+      var f3;
+
+      var i = 1;
+      while (i < 4) {
+        var j = i;
+        fun f() { print j; }
+
+        if (j == 1) f1 = f;
+        else if (j == 2) f2 = f;
+        else f3 = f;
+
+        i = i + 1;
+      }
+
+      f1();
+      f2();
+      f3();
+    `);
+    expect(consoleMock).nthCalledWith(1, '1');
+    expect(consoleMock).nthCalledWith(2, '2');
+    expect(consoleMock).nthCalledWith(3, '3');
+  });
+
+  it('function in body', () => {
+    System.run('while (true) fun foo() {}');
+    expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'fun': Expected expression."));
+  });
+
+  it('return closure', () => {
+    System.run(`
+      fun f() {
+        while (true) {
+          var i = "i";
+          fun g() { print i; }
+          return g;
+        }
+      }
+
+      var h = f();
+      h();
+    `);
+    expect(consoleMock).lastCalledWith('i');
+  });
+  // commit
+  it('return inside', () => {});
+  it('syntax', () => {});
+  it('var in body', () => {});
 });
