daa32ff3d6a4914cac7b4c6821d3629f17f6b38a
Cam Hardy <camfhardy@gmail.com>
2025-09-05 01:13:34 -0600

    added string tests

---
+++ src/lib/tests/string.test.js
@@ -0,0 +1,47 @@
+import { afterEach, describe, it, expect, vi } from 'vitest';
+import { System } from '../system.js';
+
+describe('Strings', () => {
+  const consoleMock = vi.spyOn(console, 'log');
+
+  afterEach(() => {
+    consoleMock.mockClear();
+  });
+
+	it('error after multiline', () => {
+		expect(() => System.run(`
+			var a = "1
+			2
+			3
+			";
+
+			err;
+		`)).toThrowError("Undefined variable 'err'.");
+	});
+
+	it('literals', () => {
+		System.run(`
+			print "(" + "" + ")";
+			print "a string";
+			print "A~¶Þॐஃ";
+		`);
+		expect(consoleMock).nthCalledWith(1, "()");
+		expect(consoleMock).nthCalledWith(2, "a string");
+		expect(consoleMock).nthCalledWith(3, "A~¶Þॐஃ");
+	});
+
+	it('multi-line', () => {
+		System.run(`
+			var a = "1
+2
+3";
+			print a;
+		`);
+		expect(consoleMock).lastCalledWith("1\n2\n3");
+	});
+
+	it('unterminated', () => {
+		System.run('"this string has no close quote');
+		expect(consoleMock).lastCalledWith(expect.stringContaining('Error: Unterminated string.'));
+	});
+});
