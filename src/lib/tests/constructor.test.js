76e7f8dbe5c2f83d47c856e4c98e6c0c624c24f3
Cam Hardy <camfhardy@gmail.com>
2025-09-06 02:36:35 -0600

    added more constructor tests

--- src/lib/tests/constructor.test.js
+++ src/lib/tests/constructor.test.js
@@ -85,4 +85,90 @@ describe('Constructors', () => {
 			var foo = Foo(1, 2, 3);
 		`)).toThrowError('Expected 0 arguments but got 3.');
 	});
+
+  it('early return', () => {
+		System.run(`
+			class Foo {
+				init() {
+					print "init";
+					return;
+					print "nope";
+				}
+			}
+
+			var foo = Foo(); 
+			print foo;
+		`);
+		expect(consoleMock).nthCalledWith(1, 'init');
+		expect(consoleMock).nthCalledWith(2, 'Foo instance');
+	});
+
+  it('extra arguments', () => {
+		expect(() => System.run(`
+			class Foo {
+				init(a, b) {
+					this.a = a;
+					this.b = b;
+				}
+			}
+
+			var foo = Foo(1, 2, 3, 4);
+		`)).toThrowError('Expected 2 arguments but got 4.');
+	});
+
+  it('init not method', () => {
+		System.run(`
+			class Foo {
+				init(arg) {
+					print "Foo.init(" + arg + ")";
+					this.field = "init";
+				}
+			}
+
+			fun init() {
+				print "not initializer";
+			}
+
+			init();
+		`);
+		expect(consoleMock).lastCalledWith('not initializer');
+	});
+
+  it('missing arguments', () => {
+		expect(() => System.run(`
+			class Foo {
+				init(a, b) {}
+			}
+
+			var foo = Foo(1);
+		`)).toThrowError('Expected 2 arguments but got 1.');
+	});
+
+  it('return in nested function', () => {
+		System.run(`
+			class Foo {
+				init() {
+					fun init() {
+						return "bar";
+					}
+						print init();
+				}
+			}
+
+			print Foo();
+		`);
+		expect(consoleMock).nthCalledWith(1, 'bar');
+		expect(consoleMock).nthCalledWith(2, 'Foo instance');
+	});
+
+  it('return value', () => {
+		System.run(`
+			class Foo {
+				init() {
+					return "result";
+				}
+			}
+		`);
+		expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'return': Can't return a value from an initializer."));
+	});
 });
