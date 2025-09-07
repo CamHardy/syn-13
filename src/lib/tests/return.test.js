0d3a03d822825ad4b8521b9b07e37a44ea405229
Cam Hardy <camfhardy@gmail.com>
2025-09-07 14:33:55 -0600

    added more return tests

--- src/lib/tests/return.test.js
+++ src/lib/tests/return.test.js
@@ -40,4 +40,47 @@ describe('Return', () => {
 		`);
 		expect(consoleMock).lastCalledWith('ok');
 	});
+	
+  it('at top level', () => {
+		System.run('return "wat";');
+		expect(consoleMock).lastCalledWith(expect.stringContaining("Error at 'return': Can't return from top-level code."));
+	});
+
+  it('in function', () => {
+		System.run(`
+			fun f() {
+				return "ok";
+				print "bad";
+			}
+			
+			print f();
+		`);
+		expect(consoleMock).lastCalledWith('ok');
+	});
+
+  it('in method', () => {
+		System.run(`
+			class Foo {
+				method() {
+					return "ok";
+					print "bad";
+				}
+			}
+			
+			print Foo().method();
+		`);
+		expect(consoleMock).lastCalledWith('ok');
+	});
+
+  it('return nil if no value', () => {
+		System.run(`
+			fun f() {
+				return;
+				print "bad";
+			}
+
+			print f();
+		`);
+		expect(consoleMock).lastCalledWith('nil');
+	});
 });
