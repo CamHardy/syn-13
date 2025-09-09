011c532a74b821cbf4ce84333f865cac569f643e
Cam Hardy <camfhardy@gmail.com>
2025-09-09 16:12:20 -0600

    added field get tests

--- src/lib/tests/field.test.js
+++ src/lib/tests/field.test.js
@@ -38,6 +38,64 @@ describe('Fields', () => {
 			foo.bar();
 		`)).toThrowError('Can only call functions and classes.');
 	});
+
+  it('get and set method', () => {
+		System.run(`
+			class Foo {
+				method(a) {
+					print "method";
+					print a;
+				}
+
+				other(a) {
+					print "other";
+					print a;
+				}
+			}
+				
+			var foo = Foo();
+			var method = foo.method;
+
+			foo.method = foo.other;
+			foo.method(1);
+
+			method(2);
+		`);
+		expect(consoleMock).nthCalledWith(1, 'other');
+		expect(consoleMock).nthCalledWith(2, '1');
+		expect(consoleMock).nthCalledWith(3, 'method');
+		expect(consoleMock).nthCalledWith(4, '2');
+	});
+
+  it('get on bool', () => {
+		expect(() => System.run('true.foo;')).toThrowError('Only instances have properties.');
+	});
+
+  it('get on class', () => {
+		expect(() => System.run(`
+			class Foo {}
+			Foo.bar;
+		`)).toThrowError('Only instances have properties.');
+	});
+
+  it('get on function', () => {
+		expect(() => System.run(`
+			fun foo() {}
+			foo.bar;
+		`)).toThrowError('Only instances have properties.');
+	});
+
+  it('get on nil', () => {
+		expect(() => System.run('nil.foo;')).toThrowError('Only instances have properties.');
+	});
+
+  it('get on num', () => {
+		expect(() => System.run('123.foo;')).toThrowError('Only instances have properties.');
+	});
+
+  it('get on string', () => {
+		expect(() => System.run('"str".foo;')).toThrowError('Only instances have properties.');
+	});
 	
   it('undefined', () => {
 		expect(() => System.run(`
