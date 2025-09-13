afbcc3d55753374cca3381b1d9795018c50c1706
Cam Hardy <camfhardy@gmail.com>
2025-09-13 12:00:12 -0600

    added nil test

---
+++ src/lib/tests/nil.test.js
@@ -0,0 +1,15 @@
+import { afterEach, describe, it, expect, vi } from 'vitest';
+import { System } from '../system.js';
+
+describe('Nil', () => {
+  const consoleMock = vi.spyOn(console, 'log');
+
+  afterEach(() => {
+    consoleMock.mockClear();
+  });
+
+  it('literal', () => {
+		System.run('print nil;');
+		expect(consoleMock).lastCalledWith('nil');
+	});
+});
