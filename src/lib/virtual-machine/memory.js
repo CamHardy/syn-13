5cba196c19e57ff003f2b58db6aa479f258ed360
Cam Hardy <camfhardy@gmail.com>
2025-11-24 21:39:30 -0700

    Gray object processing

--- src/lib/virtual-machine/memory.js
+++ src/lib/virtual-machine/memory.js
@@ -3,8 +3,8 @@ import { DEBUG_LOG_GC } from './common.js';
 import { IS_OBJ, AS_OBJ, OBJ_VAL } from './value.js';
 import { markTable } from "./table.js";
 import { markCompilerRoots } from "./compiler.js";
-/** @import { Value } from "./value.js" */
-/** @import { Obj } from "./object.js" */
+/** @import { Value, ValueArray } from "./value.js" */
+/** @import { Obj, ObjClosure, ObjFunction, ObjUpvalue } from "./object.js" */
 
 /** @param { number } capacity */
 export function growCapacity(capacity) {
@@ -29,9 +29,10 @@ export function freeObjects() {
 	VM.objects = null;
 }
 
-/** @param { Obj } object */
+/** @param { Obj | null } object */
 export function markObject(object) {
 	if (object === null) return;
+	if (object.isMarked) return;
 
 	if (DEBUG_LOG_GC) {
 		console.log(`${object} mark`);
@@ -66,12 +67,59 @@ function markRoots() {
 	markCompilerRoots();
 }
 
+/** @param { ValueArray } array */
+function markArray(array) {
+	for (let i = 0; i < array.count; i++) {
+		markValue(array.values[i]);
+	}
+}
+
+/** @param { Obj } object */
+function blackenObject(object) {
+	if (DEBUG_LOG_GC) {
+		console.log(`${object} blacken`);
+		console.log(OBJ_VAL(object));
+	}
+
+	switch (object.type) {
+		case 'OBJ_CLOSURE': {
+			let closure = /** @type { ObjClosure } */ (object);
+			markObject(closure.function);
+
+			for (let i = 0; i < closure.upvalueCount; i++) {
+				markObject(closure.upvalues[i]);
+			}
+			break;
+		}
+		case 'OBJ_FUNCTION': {
+			let func = /** @type { ObjFunction } */ (object);
+			markObject(func.name);
+			markArray(func.chunk.constants);
+			break;
+		}
+		case 'OBJ_UPVALUE':
+			markValue(/** @type { ObjUpvalue } **/(object).closed);
+			break;
+		case 'OBJ_NATIVE':
+		case 'OBJ_STRING':
+			break;
+	}
+}
+
+function traceReferences() {
+	while (VM.grayStack.length > 0) {
+		let object = /** @type { Obj } **/ (VM.grayStack.pop());
+		blackenObject(object);
+	}
+}
+
 export function collectGarbage() {
 	if (DEBUG_LOG_GC) {
 		console.log('-- gc begin');
 	}
 
 	markRoots();
+	traceReferences();
 
 	if (DEBUG_LOG_GC) {
 		console.log('-- gc end');
